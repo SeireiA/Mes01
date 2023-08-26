b948ed0fb5789165f58b056dfc0a6efded07f058
SeireiA <paverschlev.end@gmail.com>
2023-08-26 17:21:38 +0800

    one

---
+++ src/test/java/cool/liwu/mes01/Mes01ApplicationTests.java
@@ -0,0 +1,13 @@
+package cool.liwu.mes01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mes01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
