b79f564aa4bb1f2cda247f45356cfb975f28867e
SeireiA <paverschlev.end@gmail.com>
2023-08-28 08:45:36 +0800

    08280845

--- src/main/java/cool/liwu/mes01/Service/Impl/DataServiceImpl.java
+++ src/main/java/cool/liwu/mes01/Service/Impl/DataServiceImpl.java
@@ -1,13 +1,14 @@
-package cool.liwu.mes01.Service;
+package cool.liwu.mes01.Service.Impl;
 
 import cool.liwu.mes01.DAO.DataMapper;
+import cool.liwu.mes01.Service.DataService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
 import javax.xml.crypto.Data;
 
 @Service
-public class DataServiceImpl implements DataService{
+public class DataServiceImpl implements DataService {
     @Override
     public void createData(Data newData) throws NullPointerException{
         DataMapper dataMapper = null;
--- src/main/java/cool/liwu/mes01/Service/Impl/EnergyDetectionPointServiceImpl.java
+++ src/main/java/cool/liwu/mes01/Service/Impl/EnergyDetectionPointServiceImpl.java
@@ -1,12 +1,13 @@
-package cool.liwu.mes01.Service;
+package cool.liwu.mes01.Service.Impl;
 
 import cool.liwu.mes01.DAO.EnergyDetectionPointMapper;
 import cool.liwu.mes01.POJO.EnergyDetectionPoint;
+import cool.liwu.mes01.Service.EnergyDetectionPointService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class EnergyDetectionPointServiceImpl implements EnergyDetectionPointService{
+public class EnergyDetectionPointServiceImpl implements EnergyDetectionPointService {
     @Autowired
     private EnergyDetectionPointMapper pointMapper;
 
