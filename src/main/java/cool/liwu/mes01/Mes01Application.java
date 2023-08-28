214750968b2758ae3143485eb7b23e98783323ea
SeireiA <paverschlev.end@gmail.com>
2023-08-28 15:32:09 +0800

    修复启动错误，yml文件

--- src/main/java/cool/liwu/mes01/Mes01Application.java
+++ src/main/java/cool/liwu/mes01/Mes01Application.java
@@ -2,8 +2,9 @@ package cool.liwu.mes01;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "cool.liwu")
 public class Mes01Application {
 
     public static void main(String[] args) {
