1795c559e7c6a3153b9574b8f5ea6cc69294652f
SeireiA <paverschlev.end@gmail.com>
2023-08-28 09:09:03 +0800

    08280908

---
+++ src/main/java/cool/liwu/mes01/DAO/projectMaintenanceDao.java
@@ -0,0 +1,30 @@
+package cool.liwu.mes01.DAO;
+
+import cool.liwu.mes01.POJO.projectMaintenance;
+import org.apache.ibatis.annotations.*;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface projectMaintenanceDao {
+
+    @Select("SELECT * FROM project")
+    List<projectMaintenance> getAllProjects();
+
+    @Select("SELECT * FROM project WHERE id = #{id}")
+    projectMaintenance getProjectById(@Param("id") int id);
+
+    @Insert("INSERT INTO project(name, leader, start_date, end_date, status) " +
+            "VALUES(#{name}, #{leader}, #{startDate}, #{endDate}, #{status})")
+    @Options(useGeneratedKeys = true, keyProperty = "id", keyColumn = "id")
+    void createProject(projectMaintenance project);
+
+    @Update("UPDATE project SET name = #{name}, leader = #{leader}, " +
+            "start_date = #{startDate}, end_date = #{endDate}, status = #{status} " +
+            "WHERE id = #{id}")
+    void updateProject(projectMaintenance project);
+
+    @Delete("DELETE FROM project WHERE id = #{id}")
+    void deleteProject(@Param("id") int id);
+}
---
+++ src/main/java/cool/liwu/mes01/POJO/projectMaintenance.java
@@ -0,0 +1,60 @@
+package cool.liwu.mes01.POJO;
+
+import java.util.Date;
+
+public class projectMaintenance {
+    private int id;
+    private String name;
+    private String leader;
+    private Date startDate;
+    private Date endDate;
+    private String status;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLeader() {
+        return leader;
+    }
+
+    public void setLeader(String leader) {
+        this.leader = leader;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
